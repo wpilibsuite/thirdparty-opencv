23f593f8c415d1c73521d95e8a3e4a43a7749013
Thad House <thadhouse1@gmail.com>
2018-10-20 13:03:31 -0700

    Build Raspbian binaries

---
+++ arm-pi-gnueabihf.toolchain.cmake
@@ -0,0 +1,3 @@
+set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
+set(GNU_MACHINE "arm-raspbian9-linux-gnueabihf" CACHE STRING "GNU compiler triple")
+include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
