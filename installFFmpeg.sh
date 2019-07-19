3e5b8483ba4f8ebe7023cc59696eeadfcf482d9a
Nathan Mutin <32647745+nathanmutin@users.noreply.github.com>
2019-07-19 10:24:51 +0200

    MacOS + Cross compiling (#1)
    * Add MacOS ffmpeg with brew
    
    * Bash script for linux cross-compiling

---
+++ installFFmpeg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Bash script that download, cross-compile and install FFmpeg with the specified compiler
+# First argument = the cross-compiler prefix
+# Second argument = the target architecture
+# example : ./installFFmpeg.sh arm-raspbian9-linux-gnueabihf- armhf
+
+if [ "$1" = "" ]; then
+    echo "Parameter 1 is empty"
+    echo "You must specified a cross-compiler to build FFmpeg with"
+elif [ "$2" = "" ]; then
+    echo "Parameter 2 is empty"
+    echo "You must specified the target architecture"
+else
+    git clone git://source.ffmpeg.org/ffmpeg.git
+    cd ffmpeg
+    ./configure --enable-cross-compile --cross-prefix=$1 --arch=$2 --target-os=linux
+    make
+    make install
+    pkg_config=$(which pkg-config) PKG_CONFIG_PATH=/usr/local
+    cd ..
+fi
