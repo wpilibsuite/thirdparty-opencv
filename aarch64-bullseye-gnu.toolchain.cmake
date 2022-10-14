11be08ce5cfc50f52a43e74010dca1530845359d
Peter Johnson <johnson.peter@gmail.com>
2022-10-14 13:13:19 -0700

    Update to 2023 compilers

--- aarch64-bullseye-gnu.toolchain.cmake
+++ aarch64-bullseye-gnu.toolchain.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
-set(GNU_MACHINE "aarch64-bionic-linux-gnu" CACHE STRING "GNU compiler triple")
+set(GNU_MACHINE "aarch64-bullseye-linux-gnu" CACHE STRING "GNU compiler triple")
 set(ARM_LINUX_SYSROOT /usr/local/sys-root)
 include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
