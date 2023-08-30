d9c0822d07ef4307db31cfe3ca30dc0b1e75e388
Peter Johnson <johnson.peter@gmail.com>
2023-08-29 17:14:46 -0700

    Try setting sysroot for arm builds

--- arm-frc-gnueabi.toolchain.cmake
+++ arm-frc-gnueabi.toolchain.cmake
@@ -1,4 +1,5 @@
 set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
 set(GNU_MACHINE "arm-frc2023-linux-gnueabi" CACHE STRING "GNU compiler triple")
 set(SOFTFP yes)
+set(ARM_LINUX_SYSROOT /usr/local/arm-nilrt-linux-gnueabi/sysroot)
 include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
