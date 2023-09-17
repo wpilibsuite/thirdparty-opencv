8b427b69cbb814b4dd0c8d5870da6f67281ccae0
Peter Johnson <johnson.peter@gmail.com>
2023-09-17 15:39:49 -0700

    Update to OpenCV 4.8.0 (#73)
    Co-authored-by: Ryan Blue <ryanzblue@gmail.com>

--- aarch64-bullseye-gnu.toolchain.cmake
+++ aarch64-bullseye-gnu.toolchain.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
 set(GNU_MACHINE "aarch64-bullseye-linux-gnu" CACHE STRING "GNU compiler triple")
-set(ARM_LINUX_SYSROOT /usr/local/sys-root)
+set(ARM_LINUX_SYSROOT /usr/local/aarch64-linux-gnu/sysroot)
 include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
