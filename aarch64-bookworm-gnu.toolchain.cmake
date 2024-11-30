3d954f59efd8a7e03192bca26203009293077103
Thad House <ThadHouse@users.noreply.github.com>
2024-11-30 02:31:22 +0000

    Update to 2025 compilers, add systemcore (#88)

---
+++ aarch64-bookworm-gnu.toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
+set(GNU_MACHINE "aarch64-bookworm-linux-gnu" CACHE STRING "GNU compiler triple")
+set(ARM_LINUX_SYSROOT /usr/local/aarch64-linux-gnu/sysroot)
+include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
--- arm-frc-gnueabi.toolchain.cmake
+++ arm-frc-gnueabi.toolchain.cmake
@@ -1,5 +1,5 @@
 set(GCC_COMPILER_VERSION "" CACHE STRING "GCC Compiler version")
-set(GNU_MACHINE "arm-frc2024-linux-gnueabi" CACHE STRING "GNU compiler triple")
+set(GNU_MACHINE "arm-frc2025-linux-gnueabi" CACHE STRING "GNU compiler triple")
 set(SOFTFP yes)
 set(ARM_LINUX_SYSROOT /usr/local/arm-nilrt-linux-gnueabi/sysroot)
 include("${CMAKE_CURRENT_LIST_DIR}/opencv/platforms/linux/arm.toolchain.cmake")
